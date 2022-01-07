4947239ff9eb374defb32b2d7592b42a82dfedff
junior Mensah <mensahjunior13@gmail.com>
2022-01-07 10:50:33 +0000

    auto todo list api created

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('posts.urls')),
+    path('todo/', include('todos.urls')),
 ]
---
+++ todos/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.db.models.fields import BigAutoField
+
+# Create your models here.
+
+User = get_user_model()
+
+class Todo(models.Model):
+    # id = models.Field(BigAutoField, primary_key=True, auto_created=True)
+    task = models.TextField(max_length=255)
+    doDate = models.DateTimeField(null=True)
+    isDone = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
